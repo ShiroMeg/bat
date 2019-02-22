4c05732a968759645bc97bfde45c37c86bb8393f
ShiroMeg <megumi.shirosawa@gmail.com>
2019-02-22 10:47:25 +0100

    ajouter une servlet BateauServlet

---
+++ src/main/java/controller/BateauServlet.java
@@ -0,0 +1,45 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import data.Bateau;
+
+/**
+ * Servlet implementation class BateauServlet
+ */
+@WebServlet("/bateau")
+public class BateauServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BateauServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Bateau bateau = new Bateau("Queen is Mary Revenge");
+		response.getWriter()
+			.append("Bateau de Barbe Noire: ")
+			.append(bateau.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
